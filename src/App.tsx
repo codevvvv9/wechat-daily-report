7c98ce52d7606b1b9432aeab726db527e7a969af
shawnwwu <wslsdust@163.com>
2025-05-03 16:25:30 +0800

    chore: 移除被忽略的 data 目录跟踪

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import { ConfigProvider, theme } from 'antd';
 import zhCN from 'antd/locale/zh_CN';
 import DailyReport from './pages/DailyReport';
